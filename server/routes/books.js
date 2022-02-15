b4511d05f0f1b76780eaa5ddaae2e4e5b1576828
BibiHuebner <geffers.annika@gmail.com>
2022-02-15 16:02:53 +0100

    server setup complete

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,3 +1,13 @@
+//let's write a test route just to make sure everything is connected correctly.
+
 import express from "express";
+//create an instance of the express router by setting it to a variable called "router".
 
 const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.send({ msg: "Test route." });
+});
+//module.exports = router;
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 //LMS looks different: const express = require("express");
 import express from "express";
 import cors from "cors";
+import booksRoute from "./routes/books.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 // outdated: const bodyParser = require('body-parser');
-const cors = require("cors");
+//const cors = require("cors");
+
+dotenv.config();
 
 // Initialize Backend Server App
 
 const app = express();
 //important for deployment:
 const port = process.env.PORT || 5000;
+
+//Connection DB MongoDB
+//edit db:  I don't remember how I called my database
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch((err) => console.log(err));
 
 //Middleware check npm documentation
 //bodyparser: sends data in postrequest
@@ -23,7 +36,11 @@ app.use(
 
 app.use(cors());
 
+//Router
+app.use("/api/books", booksRoute);
+
 app.listen(port, () => {
   console.log("Server is running on " + port + "port");
 });
 // in package.json: type: module (import express)
+app.use("/books", require("./routes/books"));
